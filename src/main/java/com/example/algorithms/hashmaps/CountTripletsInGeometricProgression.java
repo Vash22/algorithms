871503604fc55b206cd1b780a5cf714ced47fcd8
kzyskowski <karol.zyskowski@lppsa.com>
2021-02-21 23:25:43 +0100

    CountTriples still not working.

--- src/main/java/com/example/algorithms/hashmaps/CountTripletsInGeometricProgression.java
+++ src/main/java/com/example/algorithms/hashmaps/CountTripletsInGeometricProgression.java
@@ -5,19 +5,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CountTriplets {
+public class CountTripletsInGeometricProgression {
 
-	@Deprecated // need to fix algorithm
 	public long countTriplets(List<Long> arr, long r) {
 		Map<Integer, List<Long>> triples = new HashMap<>();
-
+		Map<Integer, List<Long>> triplesDouble = new HashMap<>();
+		Long lastSeenValue = null;
 		int counter = 0;
-		for (int i = 0; i < arr.size(); i++) {
+		int j = -1;
+		for (int i = 0; i < arr.size(); i++, j--) {
 			List<Long> newTriple = new ArrayList<>();
 			Long currentArrayValue = arr.get(i);
 			newTriple.add(currentArrayValue);
 			triples.put(i, newTriple);
 			for (Map.Entry<Integer, List<Long>> entry : triples.entrySet()) {
+
 				if (entry.getValue().size() == 1) {
 					Long first = entry.getValue().get(0);
 					long multipliedFirst = first * r;
@@ -26,6 +28,9 @@ public class CountTriplets {
 					}
 				}
 				if (entry.getValue().size() == 2) {
+					if (currentArrayValue.equals(lastSeenValue)) {
+						triplesDouble.put(j, new ArrayList<>(entry.getValue()));
+					}
 					Long second = entry.getValue().get(1);
 					long multipliedFirst = second * r;
 					if (currentArrayValue == multipliedFirst) {
@@ -33,7 +38,9 @@ public class CountTriplets {
 						counter++;
 					}
 				}
+				lastSeenValue = currentArrayValue;
 			}
+			triples.putAll(triplesDouble);
 		}
 		return counter;
 	}
--- src/test/java/com/example/algorithms/hashmaps/ContTripletsTest.java
+++ src/test/java/com/example/algorithms/hashmaps/ContTripletsTest.java
@@ -8,19 +8,63 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ContTripletsTest {
 
-
 	@Test
-	@Deprecated // need to fix algorithm
 	public void countTriplets_case1() {
 		// given
 		List<Long> input = List.of(1L, 5L, 5L, 25L, 125L);
 		long rotation = 5;
 
 		// when
-		CountTriplets countTriplets = new CountTriplets();
-		long triplets = countTriplets.countTriplets(input, rotation);
+		CountTripletsInGeometricProgression countTripletsInGeometricProgression = new CountTripletsInGeometricProgression();
+		long triplets = countTripletsInGeometricProgression.countTriplets(input, rotation);
 
 		// then
 		assertThat(triplets).isEqualTo(4);
+	}
+
+	@Test
+	public void countTriplets_case2() {
+		// given
+		List<Long> input = List
+				.of(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L,
+						1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L,
+						1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L);
+		long rotation = 1;
+		assertThat(input).hasSize(100);
+
+		// when
+		CountTripletsInGeometricProgression countTripletsInGeometricProgression = new CountTripletsInGeometricProgression();
+		long triplets = countTripletsInGeometricProgression.countTriplets(input, rotation);
+
+		// then
+		assertThat(triplets).isEqualTo(161700);
+	}
+
+	@Test
+	public void countTriplets_case3() {
+		// given
+		List<Long> input = List.of(1L, 3L, 9L, 9L, 27L, 81L);
+		long rotation = 3;
+
+		// when
+		CountTripletsInGeometricProgression countTripletsInGeometricProgression = new CountTripletsInGeometricProgression();
+		long triplets = countTripletsInGeometricProgression.countTriplets(input, rotation);
+
+		// then
+		assertThat(triplets).isEqualTo(6);
+	}
+
+	@Test
+	public void countTriplets_case4() {
+		// given
+		List<Long> input = List.of(1L, 2L, 2L, 4L);
+		long rotation = 2;
+
+		// when
+		CountTripletsInGeometricProgression countTripletsInGeometricProgression = new CountTripletsInGeometricProgression();
+		long triplets = countTripletsInGeometricProgression.countTriplets(input, rotation);
+
+		// then
+		assertThat(triplets).isEqualTo(2);
 	}
 }
